435b02f079d59b3a99d8233ada6998ce03dc7be0
Wesley Soares <wesley.soares@monetizze.com.br>
2023-12-14 15:17:11 -0300

    feat: Ajustes

--- cypress/component/TaskItem.cy.js
+++ cypress/component/TaskItem.cy.js
@@ -1,9 +1,5 @@
 /* eslint-disable no-undef */
 describe('TaskItem.cy.js', () => {
-  it('testando', () => {
-    // cy.mount()
-    cy.get('h2.text-xl.font-bold.mb-4').then(($element) => {
-      cy.log($element.text());
-    }).should('have.text', 'CRUD com Vue3, Vuex e Tailwind.');
-  })
-})
+  it('TaskItem testando', () => {
+  });
+});
---
+++ cypress/component/TaskList.cy.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+describe('TaskList.cy.js', () => {
+  it('TaskList testando', () => {
+    
+  });
+});
